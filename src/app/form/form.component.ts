e60e7e819f6f6935626ebf2d49cbdf4437324274
AparnaShankar <shanaparna@gmail.com>
2018-10-03 01:36:16 +0530

    first commit

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -10,33 +10,16 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class FormComponent implements OnInit {
 
   fg: FormGroup;
-  submitted: boolean=false;
-  saved: boolean=false;
   requiredError: String=' This field is required.';
-  save: any;
   name: String='';
-  employee_id: String='';
-  job_title: String='';
-  department: String='';
 
   constructor(private fb: FormBuilder) {
     this.fg = this.fb.group({
     name: ['', Validators.required],
-    employee_id: ['', Validators.required],
-    job_title: ['', Validators.required],
-    department: ['', Validators.required]
     })
    }
 
   ngOnInit() {
-  }
-
-  save_as_draft(save)
-  {
-    this.name = save.name;
-    this.employee_id = save.employee_id;
-    console.log(save.name);
-    console.log(save.employee_id);
   }
 
 }
